341945edfec24ecc18525aa60e9fa72ed275bc62
Stellar <starsphere@foxmail.com>
2025-09-15 13:57:26 +0800

    delete servo task and set rgb brightness to 127

--- test/full_power_wifi/full_power_wifi.ino
+++ test/full_power_wifi/full_power_wifi.ino
@@ -15,7 +15,7 @@
 
 #define RGB_PIN 33
 #define NUM_RGB_LEDS 9
-#define RGB_BRIGHTNESS 255
+#define RGB_BRIGHTNESS 127
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 CRGB leds[NUM_RGB_LEDS];
@@ -58,19 +58,6 @@ CRGB leds[NUM_RGB_LEDS];
 #define RR_MOTOR_FWD_PWM 17
 #define RR_MOTOR_REV_PWM 18
 
-#define SERVO1_PIN 48
-#define SERVO2_PIN 47
-#define SERVO_MIN_PULSE_US 500
-#define SERVO_MAX_PULSE_US 2500
-#define SERVO1_MIN_ANGLE 0
-#define SERVO1_MAX_ANGLE 150
-#define SERVO2_MIN_ANGLE 0
-#define SERVO2_MAX_ANGLE 180
-#define SERVO1_RESET_ANGLE (SERVO1_MAX_ANGLE + SERVO1_MIN_ANGLE) / 2
-#define SERVO2_RESET_ANGLE (SERVO1_MAX_ANGLE + SERVO1_MIN_ANGLE) / 2
-#define SERVO1_MID_ANGLE (SERVO1_MAX_ANGLE + SERVO1_MIN_ANGLE) / 2
-#define SERVO2_MID_ANGLE (SERVO1_MAX_ANGLE + SERVO1_MIN_ANGLE) / 2
-
 #define WIFI_AP_SSID "ESP32_S3_AP"
 
 WebServer server(80);
@@ -103,52 +90,6 @@ SensorData_t g_sensorData;
 SemaphoreHandle_t g_sensorDataMutex;
 bool g_isAccelerometerPresent = false;
 bool g_isK210Connected = false;
-
-hw_timer_t *g_servo_timer = NULL;
-portMUX_TYPE g_servo_timer_mux = portMUX_INITIALIZER_UNLOCKED;
-#define SERVO_TIMER_FREQUENCY 1000000
-#define SERVO_TIMER_TICK_US 50
-#define SERVO_PWM_PERIOD_MS 20
-#define SERVO_PWM_PERIOD_TICKS (SERVO_PWM_PERIOD_MS * 1000 / SERVO_TIMER_TICK_US)
-volatile uint16_t g_servo1_pulse_ticks, g_servo2_pulse_ticks;
-
-void ARDUINO_ISR_ATTR on_servo_timer()
-{
-    portENTER_CRITICAL_ISR(&g_servo_timer_mux);
-    static uint16_t counter = 0;
-    if (counter == 0)
-    {
-        digitalWrite(SERVO1_PIN, HIGH);
-        digitalWrite(SERVO2_PIN, HIGH);
-    }
-    if (counter == g_servo1_pulse_ticks)
-    {
-        digitalWrite(SERVO1_PIN, LOW);
-    }
-    if (counter == g_servo2_pulse_ticks)
-    {
-        digitalWrite(SERVO2_PIN, LOW);
-    }
-    counter++;
-    if (counter >= SERVO_PWM_PERIOD_TICKS)
-    {
-        counter = 0;
-    }
-    portEXIT_CRITICAL_ISR(&g_servo_timer_mux);
-}
-
-void Servos_SetAngle(uint8_t servo_num, uint8_t angle)
-{
-    angle = constrain(angle, 0, 180);
-    uint32_t pulse_us = map(angle, 0, 180, SERVO_MIN_PULSE_US, SERVO_MAX_PULSE_US);
-    uint16_t pulse_ticks = pulse_us / SERVO_TIMER_TICK_US;
-    portENTER_CRITICAL(&g_servo_timer_mux);
-    if (servo_num == 1)
-        g_servo1_pulse_ticks = pulse_ticks;
-    else if (servo_num == 2)
-        g_servo2_pulse_ticks = pulse_ticks;
-    portEXIT_CRITICAL(&g_servo_timer_mux);
-}
 
 void controlWheel(uint8_t fwdPin, uint8_t revPin, int speed)
 {
@@ -210,22 +151,7 @@ void vMotorTask(void *pvParameters)
     }
 }
 
-void vServoTask(void *pvParameters)
-{
-    (void)pvParameters;
-    Serial.println("Servo Task started.");
-    uint8_t servo1_angle[] = {SERVO1_MIN_ANGLE, SERVO1_MID_ANGLE, SERVO1_MAX_ANGLE, SERVO1_MID_ANGLE};
-    uint8_t servo2_angle[] = {SERVO2_MIN_ANGLE, SERVO2_MID_ANGLE, SERVO2_MAX_ANGLE, SERVO2_MID_ANGLE};
-    for (;;)
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            Servos_SetAngle(1, servo1_angle[i]);
-            Servos_SetAngle(2, servo2_angle[i]);
-            vTaskDelay(pdMS_TO_TICKS(3000));
-        }
-    }
-}
+
 
 void vVoiceTask(void *pvParameters)
 {
@@ -542,7 +468,6 @@ void setup()
     Serial.println("Creating FreeRTOS tasks...");
 
     xTaskCreate(vMotorTask, "MotorTask", 2048, NULL, 2, NULL);
-    xTaskCreate(vServoTask, "ServoTask", 2048, NULL, 2, NULL);
     xTaskCreate(vVoiceTask, "VoiceTask", 2048, NULL, 1, NULL);
     xTaskCreate(vRgbTask, "RgbTask", 2048, NULL, 1, NULL);
     xTaskCreate(vSensorUpdateTask, "SensorUpdateTask", 4096, NULL, 3, NULL);
